fc7c29ed69a06ad21ed696db48deb4a71b5a2ba4
Psykka <36780964+Psykka@users.noreply.github.com>
2024-02-21 06:10:04 -0300

    fix: side bar size

--- src/components/navbar/SideBar.jsx
+++ src/components/navbar/SideBar.jsx
@@ -5,7 +5,7 @@ import { Social } from './Social'
 
 export const SideBar = component$(() => {
     return (
-        <aside className="top-0 left-0 h-screen w-20 p-4 flex flex-col justify-between bg-gray-800 text-white shadow-lg">
+        <aside className="top-0 left-0 h-screen w-24 p-4 flex flex-col justify-between bg-gray-800 text-white shadow-lg">
             <div className="flex flex-col items-center gap-4">
                 <img src="https://github.com/Psykka.png" alt="Avatar" className="rounded-full" />
                 <i class="fa-solid fa-sun" />
